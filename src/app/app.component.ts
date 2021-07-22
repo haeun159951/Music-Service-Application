4a91eb03a49a11f6ba260bc46deb4dfb0cb92926
Haeun Kim <haeun159951@gmail.com>
2021-07-22 11:22:17 -0400

    as5-v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 /*********************************************************************************
- * WEB422 – Assignment 04
+ * WEB422 – Assignment 05
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
  * assignment has been copied manually or electronically from any other source (including web sites) or
  * distributed to other students.
  *
- * Name: Ha Eun Kim Student ID: 158007187 Date: Jul 15, 2021
+ * Name: Ha Eun Kim Student ID: 158007187 Date: Jul 30, 2021
  *
  ********************************************************************************/
-
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +17,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web422-a4';
+  searchString!: string;
+
+  constructor(private router: Router) {}
+
+  handleSearch(event: Event) {
+    console.log(this.searchString);
+    this.router.navigate(['/search'], {
+      queryParams: { q: this.searchString },
+    });
+    this.searchString = '';
+  }
 }
+
+//$event.target.value
